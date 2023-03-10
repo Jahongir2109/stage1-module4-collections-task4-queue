57375c96310b55848fb797a0d276c9924bd6783f
Jahongir2109 <jahongirachilov004@gmail.com>
2023-03-10 17:43:05 +0500

    fixed some routes

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -5,5 +5,7 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String>priorityQueue=new PriorityQueue<>();
+        return priorityQueue;
     }
 }
